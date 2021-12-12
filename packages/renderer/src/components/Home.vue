d8635a8e0090f72caceb32362835b43db655f4d6
jonz94 <jody16888@gmail.com>
2021-12-12 17:20:05 +0800

    🚧 WIP: work in progress...

--- packages/renderer/src/components/Home.vue
+++ packages/renderer/src/components/Home.vue
@@ -34,7 +34,7 @@ onMounted(async () => {
     <button @click="count++">count is: {{ count }}</button>
   </fieldset>
 
-  <fieldset class="p-4 m-8 border-2">
+  <fieldset class="p-4 m-8 border-2 min-w-0">
     <legend>Font list</legend>
     <p class="flex gap-2">
       <label for="font">Font:</label>
@@ -45,8 +45,11 @@ onMounted(async () => {
       <label for="size">Size:</label>
       <input id="size" v-model="size" type="range" name="size" min="12" max="72" />
     </p>
-    <pre class="m-4 p-4 bg-slate-800 text-white" :style="{ fontFamily: codeFont, fontSize: `${size}px` }">{{
-      code
-    }}</pre>
+    <pre
+      class="py-8 m-4 bg-slate-800 text-white overflow-auto"
+      :class="size < 32 ? 'px-8' : size < 48 ? 'px-10' : size < 64 ? 'px-12' : 'px-14'"
+      :style="{ fontFamily: codeFont, fontSize: `${size}px` }"
+      >{{ code }}</pre
+    >
   </fieldset>
 </template>
